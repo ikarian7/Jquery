fda195aa5fca6f586e9219b508064517b7d072d0
Scott <Scott@10.68.237.207>
2015-11-10 10:47:58 +0100

    eerste commit

---
+++ jQuery2/src/jQuery2/JQuery2Servlet.java
@@ -0,0 +1,12 @@
+package jQuery2;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JQuery2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
